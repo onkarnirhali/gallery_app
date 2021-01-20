f5ddf7fa90834881b7beb9d4aa0bc65131c23897
onkarnirhali <43208908+onkarnirhali@users.noreply.github.com>
2021-01-20 09:41:57 +0530

    We can now read all the images from the internal storage.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:storage_path/storage_path.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
 /// This is the main application widget.
 class MyApp extends StatelessWidget {
-  static const String _title = 'Flutter Code Sample';
+  static const String _title = 'Gallery📷';
 
   @override
   Widget build(BuildContext context) {
@@ -28,24 +32,49 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
   TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Photos',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Collections',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: Favourite',
-      style: optionStyle,
-    ),
-  ];
+  // static const List<Widget> _widgetOptions = <Widget>[
+  //   Text(
+  //     'Index 0: Photos',
+  //     style: optionStyle,
+  //   ),
+  //   Text(
+  //     'Index 1: Collections',
+  //     style: optionStyle,
+  //   ),
+  //   Text(
+  //     'Index 2: Favourite',
+  //     style: optionStyle,
+  //   ),
+  // ];
+
+  Future<void> getData() async {
+    var imagePath = null;
+    try {
+      imagePath = await StoragePath.imagesPath; //contains images path and folder name in json format
+    } on PlatformException {
+      var imagesPath = 'Failed to get path';
+    }
+    //print(jsonEncode(imagePath));
+    print("the folders are"+ jsonEncode(imagePath.files));
+    //print(imagePath);
+  }
 
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
+      if(_selectedIndex == 0) {
+        HapticFeedback.heavyImpact();
+        getData();
+        print("going to Photos widget");
+      }
+      if(_selectedIndex == 1) {
+        HapticFeedback.heavyImpact();
+        print("going to Collections widget");
+      }
+      if(_selectedIndex == 2) {
+        HapticFeedback.heavyImpact();
+        print("going to Favourites widget");
+      }
     });
   }
 
@@ -56,9 +85,12 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
         title: const Text('Gallery'),
       ),
       body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
+        //child: _widgetOptions.elementAt(_selectedIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.black,
+        unselectedItemColor: Colors.white70,
+        selectedItemColor: Colors.indigo,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.photo_library),
@@ -74,7 +106,6 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
           ),
         ],
         currentIndex: _selectedIndex,
-        selectedItemColor: Colors.indigo,
         onTap: _onItemTapped,
       ),
     );
